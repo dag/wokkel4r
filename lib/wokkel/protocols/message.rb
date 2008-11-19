f89b9022b753d5fad0bac735a8bfb9f0896cc8c0
Dag Odenhall <dag.odenhall@gmail.com>
2008-11-19 12:35:08 +0100

    stupid noexpandtab

--- lib/wokkel/protocols/message.rb
+++ lib/wokkel/protocols/message.rb
@@ -4,7 +4,7 @@ module Wokkel
   module Protocols
     class Message < Handler
       def on_initialize
-	@stream.add_message_callback(&method(:on_message))
+        @stream.add_message_callback(&method(:on_message))
       end
 
       def on_message(msg)
