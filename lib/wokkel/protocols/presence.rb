9cf0360f2deab53a66b2d563a7b04f85c6bb7a2e
Dag Odenhall <dag.odenhall@gmail.com>
2008-11-19 12:38:03 +0100

    stupid me (thanks :retab)

--- lib/wokkel/protocols/presence.rb
+++ lib/wokkel/protocols/presence.rb
@@ -4,7 +4,7 @@ module Wokkel
   module Protocols
     class Presence < Handler
       def on_initialize
-	@stream.add_presence_callback(&method(:on_presence))
+        @stream.add_presence_callback(&method(:on_presence))
       end
 
       def on_presence(prc)
--- lib/wokkel/protocols/roster.rb
+++ lib/wokkel/protocols/roster.rb
@@ -5,14 +5,14 @@ module Wokkel
   module Protocols
     class Roster < Handler
       def on_initialize
-	@stream.add_iq_callback(&method(:on_iq))
+        @stream.add_iq_callback(&method(:on_iq))
       end
 
       def on_iq(iq)
-	if iq.query.kind_of? Jabber::Roster::IqQueryRoster
-	  iq.query.receive_iq(iq)
-	  on_roster_get(iq.query.to_a)
-	end
+        if iq.query.kind_of? Jabber::Roster::IqQueryRoster
+          iq.query.receive_iq(iq)
+          on_roster_get(iq.query.to_a)
+        end
       end
 
       def on_roster_get(items)
